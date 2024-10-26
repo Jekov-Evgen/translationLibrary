5a45e23d89524a5499cea20f8f4130330548c558
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-26 17:47:37 +0300

    Доделал до конца

--- translationLibrary/fromBinary.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <sstream>
-#include <cstring>
-
-
-const char* binaryToDecimal(const char*);
-const char* binaryToOctal(const char*);
-const char* binaryToHexadecimal(const char*);
--- translationLibrary/fromDecimal.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <sstream>
-#include <bitset>
-#include <cstring>
-
-const char* fromDecimalToBinary(const char*);
-const char* fromDecimalToOctal(const char*);
-const char* decimalToHexadecimal(const char*);
